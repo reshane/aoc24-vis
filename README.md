2192893d300faba894842ff2e6aef4bd439410e3
reshane <shanekenny023@gmail.com>
2025-04-24 23:10:23 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Advent of Code 2024 Visualization
+
+This project compiles several workspace crates into wasm binaries and packs them into a static site.
+
+## Quick Start
+
+```bash
+npm install
+npm run serve
+```
+Navigate to `http://localhost:8080`
+
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,14 @@ const generateWasmPackPlugin = (title) => {
 }
 
 const populatePlugins = (pagesArray) => {
-  res = [];
+  res = [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            title: 'Advent of Code Visualizations',
+            chunks: [],
+            filename: 'index.html',
+        }),
+    ];
   pagesArray.forEach(page => {
     res.push(generateHtmlPlugin(page));
     res.push(generateWasmPackPlugin(page));
@@ -27,43 +34,27 @@ const populatePlugins = (pagesArray) => {
   return res;
 }
 
-// const pages = populateHtmlPlugins(['day15', 'day16']);
+
+const populateEntrypoints = (pagesArray) => {
+    entry = {};
+    pagesArray.forEach(page => {
+        entry[page.toLowerCase()] = `./${page.toLowerCase()}/index.js`;
+    })
+    return entry;
+}
+
+const pagesArray = ['day15', 'day16'];
+
+const pages = populatePlugins(pagesArray);
+const entries = populateEntrypoints(pagesArray);;
 
 module.exports = {
-    entry: {
-        day15: './day15/index.js',
-        day16: './day16/index.js'
-    },
+    entry: entries,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            title: 'Advent of Code Visualizations',
-            chunks: [],
-            filename: 'index.html',
-        }),
-        new WasmPackPlugin({
-            crateDirectory: path.resolve(__dirname, "./day15")
-        }),
-        new HtmlWebpackPlugin({
-            template: './day15/index.html',
-            title: 'Day15',
-            chunks: ['day15'],
-            filename: 'day15/index.html',
-        }),
-        new WasmPackPlugin({
-            crateDirectory: path.resolve(__dirname, "./day16")
-        }),
-        new HtmlWebpackPlugin({
-            template: './day16/index.html',
-            title: 'Day16',
-            chunks: ['day16'],
-            filename: 'day16/index.html',
-        }),
-    ],
+    plugins: pages,
     mode: 'development',
     experiments: {
         asyncWebAssembly: true
